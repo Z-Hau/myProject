bda49d5f6ff20d1aa791f70e31241f6821860cd9
Z_Hau <zihau96@hotmail.com>
2021-03-05 12:48:02 +0800

    Add files via upload

--- testing.js
+++ testing.js
@@ -1,4 +1,1 @@
-import Web3 from 'web3';
-
-const web3 = new Web3('https://stormy-peak-44729.herokuapp.com/https://ethshared.bdnodes.net?auth=3Yc6HylCOzqKeu9aQb3w4-jo60fwp7lCpa2K6D5RJyk');
-console.log(web3);
+console.log("hihihihihihi")
